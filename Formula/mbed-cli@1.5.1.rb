d72e28c061ad1ed4c654a792fce2fb82b97ad7fe
Michael Bartling <michael.bartling@arm.com>
2018-05-08 14:01:30 +0100

    Added version to formula

--- Formula/mbed-cli@1.5.1.rb
+++ Formula/mbed-cli@1.5.1.rb
@@ -3,6 +3,7 @@ class MbedCli < Formula
   homepage "https://developer.mbed.org"
   url "https://github.com/ARMmbed/mbed-cli/archive/1.5.1.tar.gz"
   sha256 "5048535a4db03f7591b1ce808213248768df9c3f16ace53b7a24656c89bbbf64"
+  version "1.5.1"
 
   #depends_on :hg
   depends_on "mercurial"
