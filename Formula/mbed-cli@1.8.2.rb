b186399473383a66e1cb9fdf69847eb5767b76be
Michael <michael.bartling@arm.com>
2018-09-19 15:31:09 +0000

    Updated release

--- Formula/mbed-cli@1.8.2.rb
+++ Formula/mbed-cli@1.8.2.rb
@@ -1,4 +1,4 @@
-class MbedCli < Formula
+class MbedCliAT182 < Formula
   desc "ARM mbed Command Line Interface"
   homepage "https://developer.mbed.org"
   url "https://github.com/ARMmbed/mbed-cli/archive/1.8.2.tar.gz"
