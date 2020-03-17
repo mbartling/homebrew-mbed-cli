4c00a43a6dd193dc73ef60ffebc8b88836451bf0
Michael <michael.bartling@arm.com>
2020-03-17 15:30:48 +0000

    Updated release

--- Formula/mbed-cli@1.10.3.rb
+++ Formula/mbed-cli@1.10.3.rb
@@ -1,4 +1,4 @@
-class MbedCli < Formula
+class MbedCliAT1103 < Formula
   desc "ARM mbed Command Line Interface"
   homepage "https://developer.mbed.org"
   url "https://github.com/ARMmbed/mbed-cli/archive/1.10.3.tar.gz"
